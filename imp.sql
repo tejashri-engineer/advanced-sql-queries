1497c70b769c33e2851967ce79115200440cbf71
tejashri-engineer <tejashreeshravagi.421@gmail.com>
2025-06-01 00:04:32 +0530

    Create imp.sql

---
+++ imp.sql
@@ -0,0 +1,212 @@
+1)
+--9
+DROP TABLE IF EXISTS customers;
+CREATE TABLE Customers (
+    CustomerID INT,
+    CustomerName VARCHAR(50)
+);
+
+drop table Purchases;
+-- Create Purchases table
+DROP TABLE IF EXISTS purchases;
+CREATE TABLE Purchases (
+    PurchaseID INT,
+    CustomerID INT,
+    ProductName VARCHAR(50),
+    PurchaseDate DATE
+);
+
+-- Insert sample data into Customers table
+INSERT INTO Customers (CustomerID, CustomerName) VALUES
+(1, 'John'),
+(2, 'Emma'),
+(3, 'Michael'),
+(4, 'Ben'),
+(5, 'John')	;
+
+-- Insert sample data into Purchases table
+INSERT INTO Purchases (PurchaseID, CustomerID, ProductName, PurchaseDate) VALUES
+(100, 1, 'iPhone', '2024-01-01'),
+(101, 1, 'MacBook', '2024-01-20'),	
+(102, 1, 'Airpods', '2024-03-10'),
+(103, 2, 'iPad', '2024-03-05'),
+(104, 2, 'iPhone', '2024-03-15'),
+(105, 3, 'MacBook', '2024-03-20'),
+(106, 3, 'Airpods', '2024-03-25'),
+(107, 4, 'iPhone', '2024-03-22'),	
+(108, 4, 'Airpods', '2024-03-29'),
+(110, 5, 'Airpods', '2024-02-29'),
+(109, 5, 'iPhone', '2024-03-22');
+
+
+/*
+Apple data analyst interview question
+
+Given two tables - Customers and Purchases, 
+where Customers contains information about 
+customers and Purchases contains information 
+about their purchases, 
+
+write a SQL query to find customers who 
+bought Airpods after purchasing an iPhone.*/
+
+select * from Customers;
+select * from Purchases;
+
+
+SELECT 
+*
+FROM customers as c
+JOIN purchases as p1
+ON c.customerid = p1.customerid
+JOIN purchases p2
+ON c.customerid = p2.customerid		
+WHERE p1.productname = 'iPhone'
+AND
+p2.productname = 'Airpods'	
+AND
+p1.purchasedate < p2.purchasedate;
+---------------------------------------------------------------
+----2
+--11
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS returns;
+
+
+-- Create the orders table
+CREATE TABLE orders (
+    order_id VARCHAR(10),
+    customer_id VARCHAR(10),
+    order_date DATE,
+    product_id VARCHAR(10),
+    quantity INT
+);
+
+-- Create the returns table
+CREATE TABLE returns (
+    return_id VARCHAR(10),
+    order_id VARCHAR(10)
+    );
+
+
+
+
+
+
+
+
+-- Insert sample records into the orders table
+INSERT INTO orders (order_id, customer_id, order_date, product_id, quantity)
+VALUES
+    ('1001', 'C001', '2023-01-15', 'P001', 4),
+    ('1002', 'C001', '2023-02-20', 'P002', 3),
+    ('1003', 'C002', '2023-03-10', 'P003', 8),
+    ('1004', 'C003', '2023-04-05', 'P004', 2),
+    ('1005', 'C004', '2023-05-20', 'P005', 3),
+    ('1006', 'C002', '2023-06-15', 'P001', 6),
+    ('1007', 'C003', '2023-07-20', 'P002', 1),
+    ('1008', 'C004', '2023-08-10', 'P003', 2),
+    ('1009', 'C005', '2023-09-05', 'P002', 3),
+    ('1010', 'C001', '2023-10-20', 'P002', 1);
+
+-- Insert sample records into the returns table
+INSERT INTO returns (return_id, order_id)
+VALUES
+    ('R001', '1001'),
+    ('R002', '1002'),
+    ('R003', '1005'),
+    ('R004', '1008'),
+    ('R005', '1007');
+
+
+
+/*
+
+Identify returning customers based on their order history. 
+Categorize customers as "Returning" if they have placed more than one return, 
+and as "New" otherwise.
+*/
+
+select * from orders;
+select * from returns;
+
+select  
+o.customer_id,
+count(r.return_id) as cnt_return,
+count(o.order_id) as cnt_orders,
+case when count(r.return_id) > 1 then 'Returning'
+else 'New' end as type_of_return
+from orders o
+left join returns r on o.order_id = r.order_id
+group by 1;
+
+----------------------------------------------------------
+--3
+--
+-- SQL Challenge Day 14/50
+
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    customer_email VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    order_amount DECIMAL(10, 2),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+INSERT INTO customers (customer_id, customer_name, customer_email) VALUES
+(1, 'John Doe', 'john@example.com'),
+(2, 'Jane Smith', 'jane@example.com'),
+(3, 'Alice Johnson', 'alice@example.com'),
+(4, 'Bob Brown', 'bob@example.com');
+
+INSERT INTO orders (order_id, customer_id, order_date, order_amount) VALUES
+(1, 1, '2024-01-03', 50.00),
+(2, 2, '2024-01-05', 75.00),
+(3, 1, '2024-01-10', 25.00),
+(4, 3, '2024-01-15', 60.00),
+(5, 2, '2024-01-20', 50.00),
+(6, 1, '2024-02-01', 100.00),
+(7, 2, '2024-02-05', 25.00),
+(8, 3, '2024-02-10', 90.00),
+(9, 1, '2024-02-15', 50.00),
+(10, 2, '2024-02-20', 75.00);
+
+
+
+/*
+
+
+You are given two tables: orders and customers. 
+The orders table contains information about orders placed by customers, including the order_id, customer_id, order_date, and order_amount. 
+
+The customers table contains information about customers, 
+including the customer_id, customer_name, and customer_email.
+
+-- Find the top 2 customers who have spent the most money across all their orders. 
+Return their names, emails, and total amounts spent.
+
+*/
+select * from customers;
+select * from orders;
+
+select customer_name,customer_email,
+sum(order_amount) as total
+from customers c
+join orders o on c.customer_id = o.customer_id
+group by c.customer_id,c.customer_name,c.customer_email
+order by sum(order_amount) desc
+limit 2
+
+------------------------------------------------
+--4
+
+---
+
