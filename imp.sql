30effe8103d5a888d8fe447faf4798f1bdf5df79
tejashri-engineer <tejashreeshravagi.421@gmail.com>
2025-06-03 22:54:01 +0530

    Update imp.sql

--- imp.sql
+++ imp.sql
@@ -207,6 +207,145 @@ limit 2
 
 ------------------------------------------------
 --4
+--17
+--
+DROP TABLE IF EXISTS customers;
+-- Creating the Customers table
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(50)
+);
 
+
+DROP TABLE IF EXISTS purchases;
+-- Creating the Purchases table
+CREATE TABLE Purchases (
+    purchase_id INT PRIMARY KEY,
+    customer_id INT,
+    product_category VARCHAR(50),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Inserting sample data into Customers table
+INSERT INTO Customers (customer_id, customer_name) VALUES
+    (1, 'Alice'),
+    (2, 'Bob'),
+    (3, 'Charlie'),
+    (4, 'David'),
+    (5, 'Emma');
+
+-- Inserting sample data into Purchases table
+INSERT INTO Purchases (purchase_id, customer_id, product_category) VALUES
+    (101, 1, 'Electronics'),
+    (102, 1, 'Books'),
+    (103, 1, 'Clothing'),
+    (104, 1, 'Electronics'),
+    (105, 2, 'Clothing'),
+    (106, 1, 'Beauty'),
+    (107, 3, 'Electronics'),
+    (108, 3, 'Books'),
+    (109, 4, 'Books'),
+    (110, 4, 'Clothing'),
+    (111, 4, 'Beauty'),
+    (112, 5, 'Electronics'),
+    (113, 5, 'Books');
+
+select * from Customers;
+select * from Purchases;
+
+--Write an SQL query to find customers who have made 
+--purchases in all product categories.
+
+select 
+c.customer_id,
+c.customer_name,
+count(distinct p. product_category) as product_cataegory 
+from Customers c
+join Purchases p on c.customer_id = p.customer_id
+group by 1, 2
+having count(distinct p. product_category) = (select count(distinct product_category) from purchases)
+------------------------------------------------------------------------
+---18
+
+CREATE TABLE hotel_bookings (
+    booking_id SERIAL PRIMARY KEY,
+    booking_date DATE,
+    hotel_name VARCHAR(100),
+    total_guests INT,
+    total_nights INT,
+    total_price DECIMAL(10, 2)
+);
+
+-- Inserting sample data for hotel bookings for 2023 and 2022
+INSERT INTO hotel_bookings (booking_date, hotel_name, total_guests, total_nights, total_price) VALUES
+    ('2023-01-05', 'Hotel A', 2, 3, 300.00),
+    ('2023-02-10', 'Hotel B', 3, 5, 600.00),
+    ('2023-03-15', 'Hotel A', 4, 2, 400.00),
+    ('2023-04-20', 'Hotel B', 2, 4, 500.00),
+    ('2023-05-25', 'Hotel A', 3, 3, 450.00),
+    ('2023-06-30', 'Hotel B', 5, 2, 350.00),
+    ('2023-07-05', 'Hotel A', 2, 5, 550.00),
+    ('2023-08-10', 'Hotel B', 3, 3, 450.00),
+    ('2023-09-15', 'Hotel A', 4, 4, 500.00),
+    ('2023-10-20', 'Hotel B', 2, 3, 300.00),
+    ('2023-11-25', 'Hotel A', 3, 2, 350.00),
+    ('2023-12-30', 'Hotel B', 5, 4, 600.00),
+    ('2022-01-05', 'Hotel A', 2, 3, 300.00),
+    ('2022-02-10', 'Hotel B', 3, 5, 600.00),
+    ('2022-03-15', 'Hotel A', 4, 2, 400.00),
+    ('2022-04-20', 'Hotel B', 2, 4, 500.00),
+    ('2022-05-25', 'Hotel A', 3, 3, 450.00),
+    ('2022-06-30', 'Hotel B', 5, 2, 350.00),
+    ('2022-07-05', 'Hotel A', 2, 5, 550.00),
+    ('2022-08-10', 'Hotel B', 3, 3, 450.00),
+    ('2022-09-15', 'Hotel A', 4, 4, 500.00),
+    ('2022-10-20', 'Hotel B', 2, 3, 300.00),
+    ('2022-11-25', 'Hotel A', 3, 2, 350.00),
+    ('2022-12-30', 'Hotel B', 5, 4, 600.00);
+
+
+/*
+-- Write a SQL query to find out each hotal best 
+performing months based on revenue */
+
+select * from hotel_bookings;
+select * from (
+select  
+EXTRACT(YEAR FROM booking_date) as year, 
+Extract(month from booking_date) as month,
+hotel_name,
+sum(total_price) as revenue,
+row_number() over(partition by EXTRACT(YEAR FROM booking_date),hotel_name order by sum(total_price) desc ) as rnk
+from hotel_bookings
+group by 1,2,3
+ORDER BY year ASC, revenue DESC)
+where rnk = 1
+
+
+
+SELECT 
+    *
+FROM (    
+    SELECT 
+        year,
+        month,
+        hotel_name,
+        revenue,
+        RANK() OVER(PARTITION BY year, hotel_name ORDER BY revenue DESC) as rn
+    FROM 
+        (
+        SELECT
+            EXTRACT(YEAR FROM booking_date) as year,
+            EXTRACT(MONTH FROM booking_date) as month,
+            hotel_name,
+            SUM(total_price) as revenue
+        FROM hotel_bookings
+        GROUP BY 1, 2, 3
+        ORDER BY year ASC, revenue DESC
+    ) as monthly_revenue
+) as subquery
+WHERE rn = 1
+-------------------------------------------------
+--
 ---
 
